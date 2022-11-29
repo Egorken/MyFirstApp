eacb29d025e5d5fc87619cdf60b4fc3f7e957d64
George <egor.musinov.01@mail.ru>
2022-11-29 16:31:08 +0500

    lr1 first download on github

---
+++ src/Timus/timus_1293.java
@@ -0,0 +1,22 @@
+package Timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class timus_1293 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        int result = (N * (A*B)) *2;
+
+        out.println(result);
+
+        out.flush();
+    }
+
+}
